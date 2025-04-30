ebcdba858f996f9b4a17e17aa7b031f6bd03a1f9
abdalroof slaam <88548714+abdalroofslaam@users.noreply.github.com>
2025-04-30 17:16:37 +0300

    Fix FileInput.vue error

--- resources/js/Components/FileInput.vue
+++ resources/js/Components/FileInput.vue
@@ -24,7 +24,7 @@ defineEmits<{
             type="file"
             class="block w-full cursor-pointer rounded-md border border-gray-300 text-sm text-gray-900 file:mr-4 file:rounded-l-md file:border-0 file:bg-gray-100 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-gray-700 hover:file:bg-gray-200 focus:outline-none dark:border-gray-600 dark:bg-gray-700 dark:text-gray-400 dark:file:bg-gray-600 dark:file:text-gray-200 dark:hover:file:bg-gray-500"
             @input="
-                $emit('update:modelValue', $event.target.files?.[0] || null)
+               $emit('update:modelValue', ($event.target as HTMLInputElement)?.files?.[0] || null)
             "
         />
         <InputError :message="error" class="mt-2" />
